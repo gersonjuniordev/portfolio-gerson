279108a593b1438101a77d3ea3e0395e19a7cf54
gersonjuniordev <gersonjunior125@gmail.com>
2025-01-28 14:47:14 -0300

    att portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-gerson">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
