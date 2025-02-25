08d5953ced804763d42fdac623f8007dafd45cbe
gersonjuniordev <gersonjunior125@gmail.com>
2025-02-24 21:24:06 -0300

    atualizações

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio-gerson/',
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
     sourcemap: true
